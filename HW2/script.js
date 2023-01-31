763ffb3db418a50f4e580c9a4ef59c3a0091f525
Avianna Soto <89880377+AviSoto@users.noreply.github.com>
2023-01-30 21:55:07 -0700

    Update script.js

--- HW2/script.js
+++ HW2/script.js
@@ -1,4 +1,5 @@
 // Author: Avianna Soto
 
-var i = 'Test \n Hello'
-console.log(i);
+console.log("Favorite Websites: \n https://www.instagram.com \n https://www.dribble.com \n https://www.discord.com");
+console.log("Favorite Games: \n The Last of Us Part II \n Life is Strange \n Dead by Daylight");
+console.log("Favorite Artists: \n Frida Kahlo whose art holds great cultural value as she is seen as a representative of Mexican and indigenous heritage, as well as a revered feminist figure.\n Marcel Duchamp who was also a pioneering female artist who, through her work, dealt with many feminist issues, including the role of women in society. \n Keith Harin whose art is viewed as reflecting the political and cultural events of his era, and conveys powerful social and political messages. He was a prominent advocate for AIDS awareness and much of his work addresses the AIDS crisis and LGBTQ rights.");
