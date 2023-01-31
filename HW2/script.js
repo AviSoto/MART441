ecf73ea1cb7d16fd30cb3587382140095afb0df6
Avianna Soto <89880377+AviSoto@users.noreply.github.com>
2023-01-30 21:27:14 -0700

    Update script.js

--- HW2/script.js
+++ HW2/script.js
@@ -1,24 +1,18 @@
 // Author: Avianna Soto
 
-/*
-Favorite Websites:
+
+
+console.log("Favorite Websites:
 https://www.instagram.com/
 https://www.dribble.com/
 https://www.discord.com/
-*/
 
-/*
 Favorite Games:
 The Last of Us Part II
 Life is Strange
 Dead by Daylight
-*/
 
-/*
 Favorite Artists:
 Frida Kahlo whose art holds great cultural value as she is seen as a representative of Mexican and indigenous heritage, as well as a revered feminist figure.
 Marcel Duchamp who was also a pioneering female artist who, through her work, dealt with many feminist issues, including the role of women in society.
-Keith Harin whose art is viewed as reflecting the political and cultural events of his era, and conveys powerful social and political messages. He was a prominent advocate for AIDS awareness and much of his work addresses the AIDS crisis and LGBTQ rights.
-*/
-
-console.log("JavaScript Rules!");
+Keith Harin whose art is viewed as reflecting the political and cultural events of his era, and conveys powerful social and political messages. He was a prominent advocate for AIDS awareness and much of his work addresses the AIDS crisis and LGBTQ rights.");
