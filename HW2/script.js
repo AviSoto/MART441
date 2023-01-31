67bb7d78ff42ee5df61fc5a0be7efc132682b319
Avianna Soto <89880377+AviSoto@users.noreply.github.com>
2023-01-30 20:49:07 -0700

    Update script.js

--- HW2/script.js
+++ HW2/script.js
@@ -1,24 +1,4 @@
 
 // Author: Avianna Soto
 
-console.log("
-
-Favorite Websites:
-
-https://www.instagram.com
-https://dribbble.com
-https://discord.com
-
-Favorite Games:
-
-The Last of Us Part II
-Life is Strange
-Dead by Daylight
-
-Favorite Artists:
-
-The Last of Us Part II
-Life is Strange
-Dead by Daylight
-
-"); 
+console.log("TEST"); 
