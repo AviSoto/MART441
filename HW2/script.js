9674341f53f1b3ed50000c7c70e1818f08c8ffe2
Avianna Soto <89880377+AviSoto@users.noreply.github.com>
2023-01-30 21:21:06 -0700

    Update script.js

--- HW2/script.js
+++ HW2/script.js
@@ -16,7 +16,9 @@ Dead by Daylight
 
 /*
 Favorite Artists:
-Frida Kahlo
+Frida Kahlo whose art holds great cultural value as she is seen as a representative of Mexican and indigenous heritage, as well as a revered feminist figure.
+Marcel Duchamp who was also a pioneering female artist who, through her work, dealt with many feminist issues, including the role of women in society.
+Keith Harin whose art is viewed as reflecting the political and cultural events of his era, and conveys powerful social and political messages. He was a prominent advocate for AIDS awareness and much of his work addresses the AIDS crisis and LGBTQ rights.
 */
 
 console.log("JavaScript Rules!");
