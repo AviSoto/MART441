5553cf49b41d7755c41cc58ff7f6684149186f15
Avianna Soto <89880377+AviSoto@users.noreply.github.com>
2023-02-06 21:04:28 -0700

    Update main.js

--- HW3/main.js
+++ HW3/main.js
@@ -9,37 +9,37 @@ function storyFunction(choice) {
     var answer8 = document.getElementById("choice8").innerHTML;
     
     if (choice == 1 && answer1 == "Approach") {
-        document.getElementById("story").innerHTML = "You walk up to the capybara. He looks at you. What do you do?";
+        document.getElementById("storyFunction(1)").innerHTML = "You walk up to the capybara. He looks at you. What do you do?";
         document.getElementById("choice1").innerHTML = "Smile at him";
         document.getElementById("choice2").innerHTML = "Take a picture";
     } else if (choice == 4 && answer3 == "Smile at him") {
-        document.getElementById("story").innerHTML = "You give him a big old cheesy smile. He smiles back at you. What is your next step?";
+        document.getElementById("storyFunction(3)").innerHTML = "You give him a big old cheesy smile. He smiles back at you. What is your next step?";
         document.getElementById("choice1").innerHTML = "Give him your orange from your lunch";
         document.getElementById("choice2").innerHTML = "Scratch his chin";
     } else if (choice == 5 && answer5 == "Give him your orange from your lunch") {
-        document.getElementById("story").innerHTML = "How did you know this is his favorite food? He eats it whole. What is your next move?";
+        document.getElementById("storyFunction(5)").innerHTML = "How did you know this is his favorite food? He eats it whole. What is your next move?";
         document.getElementById("choice1").innerHTML = "Scratch his belly";
         document.getElementById("choice2").innerHTML = "Offer him your tuna sandwich";
     } else if (choice == 6 && answer7 == "Scratch his belly") {
-        document.getElementById("story").innerHTML = "The capybara loves getting his chin scratched after having a snack. The two of you become best friends.";
+        document.getElementById("storyFunction(7)").innerHTML = "The capybara loves getting his chin scratched after having a snack. The two of you become best friends.";
         document.getElementById("choice1").innerHTML = "Go for a swim";
         document.getElementById("choice2").innerHTML = "Get a drink";
 
     }
     else if (choice == 2 && answer2 == "Run Away") {
-        document.getElementById("story").innerHTML = "You run back to your office in a cold sweat. You have escaped.. this time." + "<br>Restart?";
+        document.getElementById("storyFunction(2)").innerHTML = "You run back to your office in a cold sweat. You have escaped.. this time." + "<br>Restart?";
         document.getElementById("choice1").innerHTML = "Restart";
         document.getElementById("choice2").innerHTML = "Quit";
     } else if (choice == 4 && answer4 == "Take a picture") {
-        document.getElementById("story").innerHTML = "The capybara hates getting his picture taken. He runs away." + "<br>Restart?";
+        document.getElementById("storyFunction(4)").innerHTML = "The capybara hates getting his picture taken. He runs away." + "<br>Restart?";
         document.getElementById("choice1").innerHTML = "Restart";
         document.getElementById("choice2").innerHTML = "Quit";
     } else if (choice == 5 && answer6 == "Scratch his chin") {
-        document.getElementById("story").innerHTML = "He bites your hand and runs away." + "<br>Restart?";
+        document.getElementById("storyFunction(6)").innerHTML = "He bites your hand and runs away." + "<br>Restart?";
         document.getElementById("choice1").innerHTML = "Yes restart";
         document.getElementById("choice2").innerHTML = "No quit";
     } else if (choice == 6 && answer8 == "Offer him your tuna sandwich") {
-        document.getElementById("story").innerHTML = "He stopped eating fish last year. He glares at you and walks away." + "<br>Restart?";
+        document.getElementById("storyFunction(8)").innerHTML = "He stopped eating fish last year. He glares at you and walks away." + "<br>Restart?";
         document.getElementById("choice1").innerHTML = "Yes restart";
         document.getElementById("choice2").innerHTML = "No quit";
 
