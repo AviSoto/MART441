dadaeb919c6e0bb25a18e0d3e01429d2a51fd713
Avianna Soto <89880377+AviSoto@users.noreply.github.com>
2023-02-06 20:55:31 -0700

    Update main.js

--- HW3/main.js
+++ HW3/main.js
@@ -1,19 +1,26 @@
 function storyFunction(choice) {
     var answer1 = document.getElementById("choice1").innerHTML;
     var answer2 = document.getElementById("choice2").innerHTML;
+    var answer3 = document.getElementById("choice3").innerHTML;
+    var answer4 = document.getElementById("choice4").innerHTML;
+    var answer5 = document.getElementById("choice5").innerHTML;
+    var answer6 = document.getElementById("choice6").innerHTML;
+    var answer7 = document.getElementById("choice7").innerHTML;
+    var answer8 = document.getElementById("choice8").innerHTML;
+    
     if (choice == 1 && answer1 == "Approach") {
         document.getElementById("story").innerHTML = "You walk up to the capybara. He looks at you. What do you do?";
         document.getElementById("choice1").innerHTML = "Smile at him";
         document.getElementById("choice2").innerHTML = "Take a picture";
-    } else if (choice == 4 && answer1 == "Smile at him") {
+    } else if (choice == 4 && answer3 == "Smile at him") {
         document.getElementById("story").innerHTML = "You give him a big old cheesy smile. He smiles back at you. What is your next step?";
         document.getElementById("choice1").innerHTML = "Give him your orange from your lunch";
         document.getElementById("choice2").innerHTML = "Scratch his chin";
-    } else if (choice == 5 && answer1 == "Give him your orange from your lunch") {
+    } else if (choice == 5 && answer5 == "Give him your orange from your lunch") {
         document.getElementById("story").innerHTML = "How did you know this is his favorite food? He eats it whole. What is your next move?";
         document.getElementById("choice1").innerHTML = "Scratch his belly";
         document.getElementById("choice2").innerHTML = "Offer him your tuna sandwich";
-    } else if (choice == 6 && answer1 == "Scratch his belly") {
+    } else if (choice == 6 && answer7 == "Scratch his belly") {
         document.getElementById("story").innerHTML = "The capybara loves getting his chin scratched after having a snack. The two of you become best friends.";
         document.getElementById("choice1").innerHTML = "Go for a swim";
         document.getElementById("choice2").innerHTML = "Get a drink";
@@ -23,18 +30,22 @@ function storyFunction(choice) {
         document.getElementById("story").innerHTML = "You run back to your office in a cold sweat. You have escaped.. this time." + "<br>Restart?";
         document.getElementById("choice1").innerHTML = "Restart";
         document.getElementById("choice2").innerHTML = "Quit";
-    } else if (choice == 4 && answer2 == "Take a picture") {
+    } else if (choice == 4 && answer4 == "Take a picture") {
         document.getElementById("story").innerHTML = "The capybara hates getting his picture taken. He runs away." + "<br>Restart?";
         document.getElementById("choice1").innerHTML = "Restart";
         document.getElementById("choice2").innerHTML = "Quit";
-    } else if (choice == 5 && answer2 == "Scratch his chin") {
+    } else if (choice == 5 && answer6 == "Scratch his chin") {
         document.getElementById("story").innerHTML = "He bites your hand and runs away." + "<br>Restart?";
         document.getElementById("choice1").innerHTML = "Yes restart";
         document.getElementById("choice2").innerHTML = "No quit";
-    } else if (choice == 6 && answer2 == "Offer him your tuna sandwich") {
+    } else if (choice == 6 && answer8 == "Offer him your tuna sandwich") {
         document.getElementById("story").innerHTML = "He stopped eating fish last year. He glares at you and walks away." + "<br>Restart?";
         document.getElementById("choice1").innerHTML = "Yes restart";
         document.getElementById("choice2").innerHTML = "No quit";
+    
+    
+    
+    
     } else if (choice ==  && answer1 == "Restart") {
         document.getElementById("story").innerHTML = "Our fearless leader, 'Pugster' has entered the dog park for the first time. Scared? Yes, but willing to give it a try. Are you ready to start on adventure with Pugster?";
         document.getElementById("choice1").innerHTML = "Yes";
