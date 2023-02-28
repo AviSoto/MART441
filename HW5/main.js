312b0dc5e5671311ac8d591c09e3e567784c674d
Avianna Soto <89880377+AviSoto@users.noreply.github.com>
2023-02-27 19:27:51 -0700

    Update main.js

--- HW5/main.js
+++ HW5/main.js
@@ -1,38 +1,85 @@
-const cardArray = [
+const cardArray = () => [
 {
-    name: 'Callisto',
-    img: 'images/Callisto.jpg'
+    name: 'Callisto',imgSrc: 'images/Callisto.jpg'
 },
 {
-    name: 'Charon',
-    img: 'images/Charon.jpg'
+    name: 'Charon', imgSrc: 'images/Charon.jpg'
 },
 {
-    name: 'Dione',
-    img: 'images/Dione.jpg'
+    name: 'Dione', imgSrc: 'images/Dione.jpg'
 },
 {
-    name: 'Enceladus',
-    img: 'images/Enceladus.jpg'
+    name: 'Enceladus', imgSrc: 'images/Enceladus.jpg'
 },
 {
-    name: 'Europa',
-    img: 'images/Europa.jpg'
+    name: 'Europa', imgSrc: 'images/Europa.jpg'
 },
 {
-    name: 'Ganymede',
-    img: 'images/Ganymede.jpg'
+    name: 'Ganymede', imgSrc: 'images/Ganymede.jpg'
 },
 {
-    name: 'Mimas',
-    img: 'images/Mimas.jpg'
+    name: 'Mimas', imgSrc: 'images/Mimas.jpg'
 },
 {
-    name: 'Titan',
-    img: 'images/Titan.jpg'
+    name: 'Titan', imgSrc: 'images/Titan.jpg'
+},
+{
+    name: 'Callisto', imgSrc: 'images/Callisto.jpg'
+},
+{
+    name: 'Charon', imgSrc: 'images/Charon.jpg'
+},
+{
+    name: 'Dione', imgSrc: 'images/Dione.jpg'
+},
+{
+    name: 'Enceladus', imgSrc: 'images/Enceladus.jpg'
+},
+{
+    name: 'Europa', imgSrc: 'images/Europa.jpg'
+},
+{
+    name: 'Ganymede', imgSrc: 'images/Ganymede.jpg'
+},
+{
+    name: 'Mimas', imgSrc: 'images/Mimas.jpg'
+},
+{
+    name: 'Titan', imgSrc: 'images/Titan.jpg'
 },
 
-]
+];
+
+const randomize = () => {
+    const cardArray = getCards();
+    cardArray.sort() => Math.random() - 0.5);
+    return cardArray;
+};
+
+const cardGenerator = () => {
+   const cardArray = randomize();
+   cardArray.forEach(item => {
+    console.log(item);
+   })
+   const card = document.createElement("div");
+   const face = document.createElement("img");
+   const back = document.createElement("div");
+   card.classList = 'card';
+   face.classList = 'face';
+   back.classList = 'back';
+
+
+}
+
+cardGenerator();
+
+
+
+
+
+
+
+
 
 card1 = null;
 card2 = null;
