e7cf1399a9de925b555f8b8c6d3e1bb528093dd0
Avianna Soto <89880377+AviSoto@users.noreply.github.com>
2023-02-27 19:57:38 -0700

    HW5

--- HW5/main.js
+++ HW5/main.js
@@ -60,16 +60,17 @@ const randomize = () => {
 
 const cardGenerator = () => {
    const cardArray = randomize();
-   cardArray.forEach(item => {
+   
+   cardArray.forEach((item => {
    const card = document.createElement("div");
    const face = document.createElement("img");
    const back = document.createElement("div");
-   card.classList = 'card';
-   face.classList = 'face';
-   back.classList = 'back';
+   card.classList = "card";
+   face.classList = "face";
+   back.classList = "back";
    
-   face.src - item.imgSrc;
-   
+   face.src = item.imgSrc;
+
    selection.appendChild(card);
    card.appendChild(face);
    card.appendChild(back);
