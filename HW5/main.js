e7d708d461ad40a36e15732f9809bb2289d92a72
Avianna Soto <89880377+AviSoto@users.noreply.github.com>
2023-02-27 20:21:25 -0700

    Update main.js

--- HW5/main.js
+++ HW5/main.js
@@ -1,48 +1,47 @@
 const section = document.querySelector('selection');
 
 const cardArray = () => [
-{ name: "Callisto", imgSrc: "./images/Callisto.jpg"},
-{ name: "Charon", imgSrc: "./images/Charon.jpg"},
-{ name: "Dione", imgSrc: "./images/Dione.jpg"},
-{ name: "Enceladus", imgSrc: "./images/Enceladus.jpg"},
-{ name: "Europa", imgSrc: "./images/Europa.jpg"},
-{ name: "Ganymede", imgSrc: "./images/Ganymede.jpg"},
-{ name: "Mimas", imgSrc: "./images/Mimas.jpg"},
-{ name: "Titan", imgSrc: "./images/Titan.jpg"},
-{ name: "Callisto", imgSrc: "./images/Callisto.jpg"},
-{ name: "Charon", imgSrc: "./images/Charon.jpg"},
-{ name: "Dione", imgSrc: "./images/Dione.jpg"},
-{ name: "Enceladus", imgSrc: "./images/Enceladus.jpg"},
-{ name: "Europa", imgSrc: "./images/Europa.jpg"},
-{ name: "Ganymede", imgSrc: "./images/Ganymede.jpg"},
-{name: "Mimas", imgSrc: "./images/Mimas.jpg"},
-{ name: "Titan", imgSrc: "./images/Titan.jpg"},
-
+  { name: "Callisto", imgSrc: "./images/Callisto.jpg"},
+  { name: "Charon", imgSrc: "./images/Charon.jpg"},
+  { name: "Dione", imgSrc: "./images/Dione.jpg"},
+  { name: "Enceladus", imgSrc: "./images/Enceladus.jpg"},
+  { name: "Europa", imgSrc: "./images/Europa.jpg"},
+  { name: "Ganymede", imgSrc: "./images/Ganymede.jpg"},
+  { name: "Mimas", imgSrc: "./images/Mimas.jpg"},
+  { name: "Titan", imgSrc: "./images/Titan.jpg"},
+  { name: "Callisto", imgSrc: "./images/Callisto.jpg"},
+  { name: "Charon", imgSrc: "./images/Charon.jpg"},
+  { name: "Dione", imgSrc: "./images/Dione.jpg"},
+  { name: "Enceladus", imgSrc: "./images/Enceladus.jpg"},
+  { name: "Europa", imgSrc: "./images/Europa.jpg"},
+  { name: "Ganymede", imgSrc: "./images/Ganymede.jpg"},
+  { name: "Mimas", imgSrc: "./images/Mimas.jpg"},
+  { name: "Titan", imgSrc: "./images/Titan.jpg"},
 ];
 
 const randomize = () => {
-    const cardArray = getCards();
-    cardArray.sort(() => Math.random() - 0.5);
-    return cardArray;
+  const cardArray = getCards();
+  cardArray.sort(() => Math.random() - 0.5);
+  return cardArray;
 };
 
 const cardGenerator = () => {
-   const cardArray = randomize();
-   
-   cardArray.forEach((item => {
-   const card = document.createElement("div");
-   const face = document.createElement("img");
-   const back = document.createElement("div");
-   card.classList = "card";
-   face.classList = "face";
-   back.classList = "back";
-   
-   face.src = item.imgSrc;
+  const cardArray = randomize();
 
-        selection.appendChild(card);
-        card.appendChild(face);
-        card.appendChild(back);
-    });
+  cardArray.forEach(item => {
+    const card = document.createElement("div");
+    const face = document.createElement("img");
+    const back = document.createElement("div");
+    card.classList = "card";
+    face.classList = "face";
+    back.classList = "back";
+   
+    face.src = item.imgSrc;
+
+    selection.appendChild(card);
+    card.appendChild(face);
+    card.appendChild(back);
+  });
 };
 
 cardGenerator();
