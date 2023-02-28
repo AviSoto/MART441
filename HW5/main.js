096c277b52ad8eb5a309c9757625259887f89ff6
Avianna Soto <89880377+AviSoto@users.noreply.github.com>
2023-02-27 19:31:37 -0700

    Update main.js

--- HW5/main.js
+++ HW5/main.js
@@ -59,61 +59,18 @@ const randomize = () => {
 const cardGenerator = () => {
    const cardArray = randomize();
    cardArray.forEach(item => {
-    console.log(item);
-   })
    const card = document.createElement("div");
    const face = document.createElement("img");
    const back = document.createElement("div");
    card.classList = 'card';
    face.classList = 'face';
    back.classList = 'back';
+   selection.appendChild(card);
+   card.appendChild(face);
+   card.appendChild(back);
 
 
-}
+});
+};
 
 cardGenerator();
-
-
-
-
-
-
-
-
-
-card1 = null;
-card2 = null;
-
-var matchSound = new Audio ("sounds/correct.mp3");
-var noMatchSound = new Audio ("sounds/incorrect.mp3");
-
-    function clicked(card){
-       
-        if(card1==null)
-        {
-            card.className = "show";
-            card1 = card;
-        }
-        else if(card2==null)
-        {
-            card.className = "show";
-            card2 = card;
-            
-            setTimeout("checkMatch();", 1000);
-        }
-    }
-
-    function checkMatch(){
-      if(card1.src==card2.src){
-        matchSound.play();
-        card1 = null;
-        card2 = null;
-      }
-      else {
-        noMatchSound.play();
-       card1.className = "hide";
-       card2.className = "hide";
-        card1 = null;
-        card2 = null; 
-      }
-    }
