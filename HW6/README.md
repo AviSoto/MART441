5d3e5cf8e4514b7fd1ed12d3e61063f564fce729
Avianna Soto <89880377+AviSoto@users.noreply.github.com>
2023-03-06 21:49:29 -0700

    HW6

---
+++ HW6/README.md
@@ -0,0 +1,1 @@
+This week I had a lot of troubles. I created a game and had it working but it did not include arrays. I went back over the course as well as watched some videos and was able to start over and create what I have now.
---
+++ HW6/main.js
@@ -0,0 +1,63 @@
+const section = document.querySelector('section');
+
+const cardArray = () => [
+  { name: "Callisto", imgSrc: "./images/Callisto.jpg"},
+  { name: "Charon", imgSrc: "./images/Charon.jpg"},
+  { name: "Dione", imgSrc: "./images/Dione.jpg"},
+  { name: "Enceladus", imgSrc: "./images/Enceladus.jpg"},
+  { name: "Europa", imgSrc: "./images/Europa.jpg"},
+  { name: "Ganymede", imgSrc: "./images/Ganymede.jpg"},
+  { name: "Mimas", imgSrc: "./images/Mimas.jpg"},
+  { name: "Titan", imgSrc: "./images/Titan.jpg"},
+  { name: "Callisto", imgSrc: "./images/Callisto.jpg"},
+  { name: "Charon", imgSrc: "./images/Charon.jpg"},
+  { name: "Dione", imgSrc: "./images/Dione.jpg"},
+  { name: "Enceladus", imgSrc: "./images/Enceladus.jpg"},
+  { name: "Europa", imgSrc: "./images/Europa.jpg"},
+  { name: "Ganymede", imgSrc: "./images/Ganymede.jpg"},
+  { name: "Mimas", imgSrc: "./images/Mimas.jpg"},
+  { name: "Titan", imgSrc: "./images/Titan.jpg"},
+];
+
+const randomize = () => {
+  const cards = cardArray();
+  cards.sort(() => Math.random() - 0.5);
+  return cards;
+};
+
+const cardGenerator = () => {
+  const cardArray = randomize();
+
+  cardArray.forEach(item => {
+    const card = document.createElement("div");
+    const back = document.createElement("img");
+    const face = document.createElement("img");
+    card.classList = "card";
+    back.classList = "back";
+    face.classList = "face";
+   
+    back.src = "./images/Blank.jpg";
+    face.src = item.imgSrc;
+
+    section.appendChild(card);
+    card.appendChild(face);
+    card.appendChild(back);
+
+    card.addEventListener("click", function () {
+      card.classList.toggle("flip");
+    });
+
+    });
+};
+
+cardGenerator();
+
+function getInformation() {
+  var information = {
+    "firstname": document.getElementById("firstName").value,
+    "lastname": document.getElementById("lastName").value,
+    "age": document.getElementById("age").value
+  };
+  document.getElementById("info").innerHTML = information.firstname + ":" + information.lastname + ":" + information.age;
+  localStorage.setItem("information", JSON.stringify(information));
+}
