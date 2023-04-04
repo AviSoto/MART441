3516f1790b0625eee6ac5987bd56bdcd1884f2a9
Avianna Soto <89880377+AviSoto@users.noreply.github.com>
2023-04-03 23:21:26 -0600

    HW9

---
+++ HW9/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $.getJSON('https://api.tvmaze.com/search/shows?q=death+note', function(data, status) {
+        var text = '';
+        for (let i = 0; i < 5; i++) {
+            if (data[i] != null) {
+                text += `${data[i].show.name}<br>`;
+                if (data[i].show.image && data[i].show.image.medium != null) {
+                    text += `<img src=${data[i].show.image.medium} height="300"><br>`;
+                } else {
+                    text += `<p>No image available.</p>`;
+                }
+                text += `${data[i].show.summary}<br><br>`;
+            } else {
+                console.log("Found null data.");
+            }
+        }
+        $(".episode").html(text);
+    });
+});
