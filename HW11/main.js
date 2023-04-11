8eb517ac564a395d9136f9678af5db2630e68f05
Avianna Soto <89880377+AviSoto@users.noreply.github.com>
2023-04-11 01:27:47 -0600

    HW

--- HW11/main.js
+++ HW11/main.js
@@ -1,58 +1,29 @@
-function boundingBoxCollision(square1, square2) {
-    const square1BB = square1.getBB();
-    const square2BB = square2.getBB();
-  return square1BB.intersects(square2BB);
-}
-
 class Square {
-    constructor(x, y, height, width, color) {
-      this.x = x;
-      this.y = y;
-      this.height = height;
-      this.width = width;
-      this.color = color;
+    constructor(options) {
+      this.x = options.x;
+      this.y = options.y;
+      this.height = options.height;
+      this.width = options.width;
+      this.color = options.color;
     }
-getBB() {
-        return {
-          x1: this.x,
-          y1: this.y,
-          x2: this.x + this.width,
-          y2: this.y + this.height,
-         };
-       }
-    }
-    setX(x) {
-      this.x = x;
-    }
-    setY(y) {
-      this.y = y;
-    }
-    setHeight(height) {
-      this.height = height;
-    }
-    setWidth(width) {
-      this.width = width;
-    }
-    setColor(color) {
-      this.color = color;
-    }
-    get theX() {
+  
+    getX() {
       return this.x;
     }
-    get theY() {
+    getY() {
       return this.y;
     }
-    get theHeight() {
-      return this.height;
-    }
-    get theWidth() {
+    getWidth() {
       return this.width;
     }
-    get theColor() {
+    getHeight() {
+      return this.height;
+    }
+    getColor() {
       return this.color;
     }
-  }
-  
+}
+
   var canvas = document.getElementById("myCanvas");
   var ctx = canvas.getContext('2d');
 
@@ -60,84 +31,100 @@ getBB() {
   var y = 50;
   var x2 = 300;
   var y2 = 300;
-  var square1;
-  var square2;
 
+  var square1 = new Square({ x: 10, y: 20, height: 50, width: 50, color: "hotpink" });
+  var square2 = new Square({ x: 50, y: 100, height: 50, width: 50, color: "purple" });
+  
   $(document).ready(function () {
-    square1 = new Square(50, 50, 50, 50, "hotpink");
-    square2 = new Square(100, 100, 50, 50, "purple");
-  
+   
     drawSquare();
-    $(this).keypress(function (event) {
-      getKey(event);
-    });
+    setInterval(updateCanvas, 1000 / 60);
   });
-  
-  function drawSquare() {
-    ctx.clearRect(0, 0, 800, 600); 
-    square1.setX(x);
-    square1.setY(y);
-    ctx.fillStyle = square1.theColor;
-    ctx.fillRect(
-      square1.theX,
-      square1.theY,
-      square1.theWidth,
-      square1.theHeight
-    );
-  
-    square2.setX(x2);
-    square2.setY(y2);
-    ctx.fillStyle = square2.theColor;
-    ctx.fillRect(
-      square2.theX,
-      square2.theY,
-      square2.theWidth,
-      square2.theHeight
-    );
-  }
-  
-  
-  function moveUp() {
-    y -= 20;
-  }
-  
-  function moveDown() {
-    y += 20;
-  }
-  function moveLeft() {
-    x -= 20;
-  }
-  function moveRight() {
-    x += 20;
-  }
-  
-  function hasCollided(object1, object2) {
-    const object1BB = object1.getBB();
-    const object2BB = object2.getBB();
-    return !(
-      object1BB.y2 < object2BB.y1 ||
-      object1BB.y1 > object2BB.y2 ||
-      object1BB.x2 < object2BB.x1 ||
-      object1BB.x1 > object2BB.x2
-    );
-  }
-  
-  function getKey(event) {
-    var didCollide = hasCollided(square1, square2);
-    if (didCollide) {
-      window.alert("uhoh! you crashed!");
-    }
-    var char = event.which || event.keyCode;
-    var actualLetter = String.fromCharCode(char);
-    if (actualLetter == "w") {
-      moveUp();
-    } else if (actualLetter == "s") {
-      moveDown();
-    } else if (actualLetter == "d") {
-      moveRight();
-    } else if (actualLetter == "a") {
-      moveLeft();
-    }
+
+  function updateCanvas() {
     drawSquare();
+    moveSquare2random();
+
+    var didCollide = hasCollided(square1, square2);
+
+    if (didCollide) {
+        canvas.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";
+      
+        square1.width = square1.width - 1;
+        square1.height = square1.height - 1;
+        square2.width = square2.width + 1;
+        square2.height = square2.height + 1;
+    }
+  }
+
+function drawSquare() {
+  ctx.clearRect(0, 0, 800, 600);
+  ctx.fillStyle = square1.getColor();
+  ctx.fillRect(
+    square1.getX(),
+    square1.getY(),
+    square1.getWidth(),
+    square1.getHeight()
+  );
+
+  ctx.fillStyle = square2.getColor();
+  ctx.fillRect(
+    square2.getX(),
+    square2.getY(),
+    square2.getWidth(),
+    square2.getHeight()
+  );   
+}
+var xVelocity;
+var yVelocity;
+
+xVelocity = Math.floor((Math.random() * (4-2)+2));
+yVelocity = Math.floor((Math.random() * (4-2)+2));
+
+function moveSquare2random() {
+  if (square2.x + square2.width > canvas.width) {
+    square2.x = canvas.width - square2.width; 
+  }
+  if (square2.x <= 0) { 
+    xVelocity *= -1;
+    xVelocity += Math.floor((Math.random()))
+  }
+   else if (square2.x + square2.width >= canvas.width) {
+    xVelocity *= -1; 
+    xVelocity += -Math.floor((Math.random()))
+    
   }
   
+  if (square2.y <= 0 || square2.y + square2.height >= canvas.height) {
+    yVelocity *= -1;
+ 
+  }
+
+  square2.x += xVelocity;
+  square2.y += yVelocity;
+  drawSquare(); 
+}
+
+window.addEventListener("keydown", keyHandlerDown);
+window.addEventListener("keyup", keyHandlerDown);
+
+function keyHandlerDown(e) {
+ if (e.keyCode == 65 && square1.x > 0) {
+    square1.x -= 10;
+  } else if (e.keyCode == 83 && square1.y < canvas.height - square1.height) {
+    square1.y += 10;
+  } else if (e.keyCode == 87 && square1.y > 0) {
+    square1.y -= 10; 
+  } else if (e.keyCode == 68 && square1.x < canvas.width - square1.width) {
+    square1.x += 10;   
+  }
+}
+
+function hasCollided(object1, object2) {
+    return !(
+      object1.y + object1.height < object2.y ||
+      object1.y > object2.y + object2.height ||
+      object1.x + object1.width < object2.x ||
+      object1.x > object2.x + object2.width
+    );
+  }
