29d247719a3ebf870ec376be26705421dd9ae977
Avianna Soto <89880377+AviSoto@users.noreply.github.com>
2023-05-10 18:52:48 -0600

    final

--- Final/main.js
+++ Final/main.js
@@ -5,3 +5,20 @@ const errorMessage = document.getElementById('error-message');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   
+const text = "Welcome to my website";
+let index = 0;
+
+function typeText() {
+  document.getElementById("typing-text").innerHTML = text.slice(0, index);
+  index++;
+  if (index > text.length) {
+    index = 0;
+  }
+  setTimeout(typeText, 50);
+}
+
+typeText();
+
+$(document).ready(function() {
+    $('#myElement').fadeIn(1000);
+});
